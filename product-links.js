6f6fe170eabe5184d4a9fd1ff499dc6922c3e699
jacobdurrah <jacob.e.durrah@gmail.com>
2026-08-20 20:25:44 -0400

    Render Used together first on product boxes

--- product-links.js
+++ product-links.js
@@ -1,8 +1,9 @@
 window.IDP_productLinks=function(item,esc){
 function ok(h){h=String(h||"");return /^https:\/\/[A-Za-z0-9._~:/?#\[\]@!$&'()*+,;=%-]+$/.test(h)?h:""}
 function a(o){if(!o||!o.n)return"";var h=ok(o.href);return h?'<a href="'+h+'" target="_blank" rel="noopener">'+esc(o.n)+"</a>":esc(o.n)}
-function list(arr){return (arr||[]).map(a).filter(Boolean).join("<br>")}
-var html="";
+function list(arr,sep){return (arr||[]).map(a).filter(Boolean).join(sep||"<br>")}
+var html="",t=item.together,p=t&&list(t.parts," + ");
+if(p){html+="<h3>Used together</h3><p>"+p+"</p>";if(t.why)html+="<p>"+esc(t.why)+"</p>"}
 if(item.latest) html+="<h3>Latest</h3><p>"+a(item.latest)+"</p>";
 var c=list(item.common); if(c) html+="<h3>Most used</h3><p>"+c+"</p>";
 var see=list(item.see), buy=list(item.buy);
