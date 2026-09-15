6ce019f04f714fddf272fce7739d42e82bdd1f8d
jacobdurrah <jacob.e.durrah@gmail.com>
2026-09-15 12:38:30 -0400

    Agents: add Ship agent / FDE enterprise story registry

---
+++ a-data-stories.js
@@ -0,0 +1,4 @@
+window.IDP_SCENARIOS = [
+  {id:"ship", n:"Ship agent", steps:["step1","step2","step3","step4","step5","step6","step7","step8","step9","step10","step11","step12"]},
+  {id:"fde", n:"FDE enterprise", steps:["fde-tenant","fde-rbac","fde-creds","fde-tags","fde-workflow","fde-gates","fde-boards","fde-pave","fde-slo"]}
+];
