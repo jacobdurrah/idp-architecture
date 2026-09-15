b9439e4eced96c8a6ab12d87157ee9d8f735bc4e
jacobdurrah <jacob.e.durrah@gmail.com>
2026-09-15 12:38:39 -0400

    Agents: tag ship steps with story=ship

---
+++ a-data-story-tag.js
@@ -0,0 +1,3 @@
+(function(D){
+["step1","step2","step3","step4","step5","step6","step7","step8","step9","step10","step11","step12"].forEach(function(k){if(D[k])D[k].story="ship"});
+})(window.IDP_DATA);
